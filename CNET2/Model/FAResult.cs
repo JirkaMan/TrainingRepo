967225f551c7fe0af93877bbfe5e04920cfeb8ba
Jiří Mancl <jiri.mancl@technodat.cz>
2022-06-13 11:49:48 +0200

    Vytvorení třídy frekvenční analýzy Niový způsob zapsání stringu

---
+++ CNET2/Model/FAResult.cs
@@ -0,0 +1,34 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Výsledek frekvenční analýzi pro jeden zdroj (soubor nebo url)
+    /// </summary>
+    public class FAResult
+    {
+        /// <summary>
+        /// Zdroj textu
+        /// </summary>
+        public string Source { get; set; }
+        
+        /// <summary>
+        /// Typ zdroje (url,file,db ...)
+        /// </summary>
+        public SourceType SourceType { get; set; }
+
+        /// <summary>
+        /// Vysledna frekvencni analyaza slov
+        /// </summary>
+        public Dictionary<string, int> Words { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Source} {Words?.Count}";   //  Zápis stringu od .NET5
+        }
+    }
+
+    public enum SourceType
+    {
+        URL,
+        FILE
+    }
+}
