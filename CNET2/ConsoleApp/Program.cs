d99163d6ba51f36ae0e5a33bc6a0e4f7a6390657
Jiří Mancl <jiri.mancl@technodat.cz>
2022-06-14 08:47:54 +0200

    LINQ cvicenií zabaleno do metody

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -5,71 +5,7 @@ using System.Linq;
 
 Console.WriteLine("Hello, World!");
 
-
-int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
-
-//  Where - FILTRACE
-//var result = numbers.Where(n => n > 0 && n <= 99);
-
-//var result2 = numbers.Where(n => n > 0)
-//                     .Where(n => n <= 99);
-
-//  ORDERBY
-//var result = numbers.OrderBy(n => n);
-
-//  AGREGACE - Min, Max, Sum,Avg, Count
-//var number1 = numbers.Max();
-//var number2 = numbers.Min();
-//var number3 = numbers.Average();
-//var number4 = numbers.Sum();
-
-
-// TAKE SKIP
-//var result = numbers.TakeWhile(n => n > 1000);
-
-//  SELECT + transformace
-//var result = numbers.Select(n => Math.Abs(n));
-
-
-
-//  *********************************************************************************
-//  CVICENI
-//  *********************************************************************************
-
-//  1. Zjistit pocet kladnych hodnot v numbers
-//var result = numbers.Where(x => x > 0).Count();
-
-
-
-
-//  2. Ignorujte nejvetsi a nejmensi cislo a spocitejte prumer
-//var result = numbers.OrderBy(n => n).Skip(1).SkipLast(1).Average();
-
-
-
-
-//  3.  Spocitat pocet lichych a sudych
-
-//var countEven = numbers.Where(c => c % 2 != 0).Count();
-//var countOdd = numbers.Where(c => c % 2 == 0).Count();
-
-//Console.WriteLine($"Suda: {countEven} , Licha: {countOdd}");
-
-
-//  4.  vypište čísla v poli numbers jako slova
-var numbers2 = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-var strings = new[] { "zero", "one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine" };
-
-var result = numbers2.Select(n => strings[n]);
-
-
-Console.WriteLine(string.Join(", ", result));
-
-//foreach (var item in result)
-//{
-//    Console.WriteLine(item);
-//}
+//LINQcviceni();
 
 
 
@@ -115,3 +51,70 @@ static void FreqWords()
     }
 }
 
+static void LINQcviceni()
+{
+    int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
+
+    //Where - FILTRACE
+    var result = numbers.Where(n => n > 0 && n <= 99);
+
+    var result2 = numbers.Where(n => n > 0)
+                         .Where(n => n <= 99);
+
+    //ORDERBY
+    var result3 = numbers.OrderBy(n => n);
+
+    //AGREGACE - Min, Max, Sum,Avg, Count
+    var number1 = numbers.Max();
+    var number2 = numbers.Min();
+    var number3 = numbers.Average();
+    var number4 = numbers.Sum();
+
+
+    //TAKE SKIP
+    var result4 = numbers.TakeWhile(n => n > 1000);
+
+    //SELECT + transformace
+    var result5 = numbers.Select(n => Math.Abs(n));
+
+
+
+    //*********************************************************************************
+    //CVICENI
+    //* ********************************************************************************
+
+    //1.Zjistit pocet kladnych hodnot v numbers
+    var result6 = numbers.Where(x => x > 0).Count();
+
+
+
+
+    //2.Ignorujte nejvetsi a nejmensi cislo a spocitejte prumer
+    var result7 = numbers.OrderBy(n => n).Skip(1).SkipLast(1).Average();
+
+
+
+
+    //3.Spocitat pocet lichych a sudych
+
+    var countEven = numbers.Where(c => c % 2 != 0).Count();
+    var countOdd = numbers.Where(c => c % 2 == 0).Count();
+
+    Console.WriteLine($"Suda: {countEven} , Licha: {countOdd}");
+
+
+    //  4.  vypište čísla v poli numbers jako slova
+    var numbers2 = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+    var strings = new[] { "zero", "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine" };
+
+    var result8 = numbers2.Select(n => strings[n]);
+
+
+    Console.WriteLine(string.Join(", ", result));
+
+    //foreach (var item in result)
+    //{
+    //    Console.WriteLine(item);
+    //}
+}
