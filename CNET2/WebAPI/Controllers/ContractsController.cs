48b1b8f2805b4b3f7974fbdd8785c34f78d3a01c
Jiří Mancl <jiri.mancl@technodat.cz>
2022-06-15 14:40:01 +0200

    Přidání WEBApi a controllerů pro objekty

---
+++ CNET2/WebAPI/Controllers/ContractsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Data;
+using Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractsController : ControllerBase
+    {
+        [HttpGet("GetAllContracts")]
+        public IEnumerable<Contract> GetContracts()
+        {
+            var dataSet = Data.Serialization.LoadFromXML(@"d:\Data\SkoleniICTpro\PersonDataset\dataset.xml");
+
+            return dataSet.SelectMany(p => p.Contracts);
+        }
+    }
+}
---
+++ CNET2/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public List<Person> GetPeople()
+        {
+            var dataSet = Data.Serialization.LoadFromXML(@"d:\Data\SkoleniICTpro\PersonDataset\dataset.xml");
+
+            return dataSet;
+        }
+
+        [HttpGet("ByEmail/{email}")]
+        public Person GetByEmail(string email)
+        {
+            var dataSet = Data.Serialization.LoadFromXML(@"d:\Data\SkoleniICTpro\PersonDataset\dataset.xml");
+
+            return dataSet.Where(p => p.Email.ToLower() == email.ToLower()).FirstOrDefault();
+        }
+    }
+}
